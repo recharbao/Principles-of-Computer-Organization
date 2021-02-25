ab7e5747b2022246d1d9ef3de51d58a831e9a3fd
rechar <884520248@qq.com>
2021-02-25 18:24:27 +0800

    改进code2将cx暂存在内存当中

---
+++ 更灵活的定位内存地址/code3.asm
@@ -0,0 +1,35 @@
+assume cs:codesg, ds:datasg
+datasg segment
+  db 'ibm             '
+  db 'dec             '
+  db 'dos             '
+  db 'vax             '
+datasg ends
+
+codesg segment
+start:
+  mov ax, datasg
+  mov ds, ax
+  mov bx, 0
+
+  mov cx, 4
+
+e:mov ds:[40H], cx
+  mov si, 0
+  mov cx, 3
+
+s:mov al, [bx+si]
+  mov al, 11011111b
+  mov [bx+si], al
+  inc si
+  loop s
+
+  add bx, 16
+  mov cx, ds:[40H]
+  loop e
+
+  mov ax, 4c00H
+  int 21H
+
+codesg ends
+end start
